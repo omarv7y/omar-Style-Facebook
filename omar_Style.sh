879e9bfa8f958119b7f56c5b9cafdd62b8f034e0
omarv7y <70105391+omarv7y@users.noreply.github.com>
2021-01-15 12:53:45 +0200

    Add files via upload

---
+++ omar_Style.sh
@@ -0,0 +1,27 @@
+#bash 
+#################
+#               #
+#               #
+#################
+clear
+xdg-open https://www.youtube.com/channel/UC7UHYAjHf-gASPWSHw6sq3A?view_as=subscriber
+sleep 9
+printf '\033[1;31m  Omar Style '
+sleep 5
+green='\e[1;32m'
+
+echo -e $green "-------------------------------------------------------"
+sleep 0.3
+echo -e $green "-      OMAR   STYLE                                   -"
+sleep 0.3
+echo -e $green "-     👍👍👍👍👍👍👍👍👍👍                            -"
+sleep 0.3
+echo -e $green "-------------------------------------------------------"
+sleep 5
+
+clear
+pip install import requests
+pip install mechanize
+
+python3 omar_Style_feacbook.py
+
